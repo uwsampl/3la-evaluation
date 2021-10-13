1ebc55c14b35bcf264c01d8c746b8a2fda4b463e
Gus Smith <guscomps@gmail.com>
2021-10-13 14:02:05 -0700

    Whoops, don't cd

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Test the 3LA version of Glenside.
-cd glenside && cargo test --no-default-features --features "tvm"
+cargo test --manifest-path glenside/Cargo.toml --no-default-features --features "tvm"
 
 # Test import of BERT into Relay.
 python3 bert_onnx.py
