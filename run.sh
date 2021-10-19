28df94edc4f54e12fc34006879d0d3c8cbf92b25
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2021-10-18 23:43:48 -0700

    Add call to end-to-end ResMLP to run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,9 @@ cargo test --manifest-path glenside/Cargo.toml --no-default-features --features 
 
 # Test import of BERT into Relay.
 python3 bert_onnx.py
+
+# Test end-to-end run of ResMLP
+pushd e2e/resmlp
+python3 trial.py --num-images 5
+python3 digest.py
+popd
