ad53a5830e3d0f06292f86c9b8702f02cb1c20b3
Gus Smith <guscomps@gmail.com>
2022-07-04 16:53:09 -0700

    Add sample script for legalizing var names

---
+++ diffing_models_from_different_frameworks/legalize-var-names.py
@@ -0,0 +1,29 @@
+"""Legalize variable names for Relay modules imported from other frameworsk. 
+
+Turn this into a script, or just edit the vars below."""
+import re
+import tvm
+from tvm import relay
+
+FILENAME = "resnet50_simplifyinference_from_tf.relay"
+MAIN_FUNC_DEF_LINE = 2
+
+file = open(FILENAME).read()
+funcdef = file.splitlines()[MAIN_FUNC_DEF_LINE]
+
+
+def f(code, find, replacewith):
+    return code.replace(find, replacewith)
+
+
+newcode = file
+matches = re.findall(r'%[a-zA-Z_\./0-9]*:', funcdef)
+for match in matches:
+    newcode = f(newcode, match[:-1],
+                match[:-1].replace('/', '_').replace('.', '_'))
+
+tvm.parser.fromtext(newcode)
+mod = tvm.parser.fromtext(newcode)
+mod = relay.transform.InferType()(mod)
+
+print(mod)
